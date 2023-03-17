919d2021cf0879da29ab3607430008e7778bcabb
TonyMike <tee.jhay1@gmail.com>
2023-03-17 10:55:11 +0100

    fix break in dropdown

--- src/components/Resuables/DropDown.vue
+++ src/components/Resuables/DropDown.vue
@@ -7,10 +7,10 @@
           : clicked === 'Partial'
           ? 'bg-blue-500 text-white'
           : clicked === 'Full'
-          ? 'bg-green-700 text-white'
+          ? 'bg-green-700 text-white '
           : 'bg-yellow-500 text-black'
       ]"
-      class="text-sm  text-center cursor-pointer   py-1 flex space-x-3 items-center rounded-full px-6  "
+      class="text-sm  text-center cursor-pointer   py-1 flex justify-between items-center rounded-full px-6  "
       @click="show = !show"
     >
       <span v-if="clicked">{{ clicked }}</span>
@@ -20,25 +20,25 @@
 
     <ul v-show="show" class="bg-white transition rounded-md top-8 shadow-md">
       <li
-        @click="handleClick('Not Invoiced')"
+        @click=";[handleClick('Not Invoiced'), (show = false)]"
         class="py-2 text-sm pl-4 w-[165px] cursor-pointer hover:bg-slate-100 text-left"
       >
         Not Invoiced
       </li>
       <li
-        @click="handleClick('Partial')"
+        @click=";[handleClick('Partial'), (show = false)]"
         class="py-2 text-sm pl-4 w-[165px] cursor-pointer hover:bg-slate-100 text-left"
       >
         Partial
       </li>
       <li
-        @click="handleClick('Full')"
+        @click=";[handleClick('Full'), (show = false)]"
         class="py-2 text-sm pl-4 w-[165px] cursor-pointer hover:bg-slate-100 text-left"
       >
         Full
       </li>
       <li
-        @click="handleClick('Send to Costing')"
+        @click=";[handleClick('Send to Costing'), (show = false)]"
         class="py-2 text-sm pl-4 w-[165px] cursor-pointer hover:bg-slate-100 text-left"
       >
         Send to Costing
